eeca5ba16ba70fcb5bcc30cc61be99408e784192
Camille Briand <camille.briand@efrei.net>
2019-02-12 19:25:58 +0100

    Ajout de JavaDoc à la classe Professeur

--- src/notesElevesProfesseurs/Professeur.java
+++ src/notesElevesProfesseurs/Professeur.java
@@ -4,19 +4,33 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+
+/**
+ * Représente un professeur.
+ *  Cette classe hérite de {@link Personne}.
+ */
 public class Professeur extends Personne
 {
     //List<String> matière;
 
 
+    /**
+     * Constructeur par défaut, instancie un {@link Professeur} à partir d'une {@link Personne} par défaut
+     */
     public Professeur()
     {
         super();
     }
 
 
-    public Professeur(String pNom, String pPrenom, Date pDateNaissance)
+    /**
+     * Instancie un {@link Professeur} à partir des informations d'une personne (nom, prénom, date de naissance)
+     * @param nom Nom de la {@link Personne} à instancier
+     * @param prenom Prénom de la {@link Personne} à instancier
+     * @param dateNaissance Date de naissance de la {@link Personne} à instancier
+     */
+    public Professeur(String nom, String prenom, Date dateNaissance)
     {
-        super(pNom, pPrenom, pDateNaissance);
+        super(nom, prenom, dateNaissance);
     }
 }
