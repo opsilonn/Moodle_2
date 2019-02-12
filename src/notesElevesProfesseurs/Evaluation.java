9c0bf99c9f580bf30a0cd9cbc3956bf6872b9ba0
Camille Briand <camille.briand@efrei.net>
2019-02-12 18:41:04 +0100

    Améliore la classe Evaluation (JavaDoc + respect des conventions)

--- src/notesElevesProfesseurs/Evaluation.java
+++ src/notesElevesProfesseurs/Evaluation.java
@@ -3,28 +3,46 @@ package notesElevesProfesseurs;
 public class Evaluation
 {
     private double note;
-    Eleve auteur;
-    Professeur correcteur;
+    private Eleve auteur;
+    private Professeur correcteur;
 
 
+    private static final double NOTE_PAR_DEFAUT = 10.0;  // #NoMagicNumbers
+
+
+    /**
+     * Constructeur par défaut.
+     * Déprécié dans le sens où il vaut mieux ne pas l'utiliser
+     */
+    @Deprecated
     public Evaluation()
     {
-        note = 10;
-        auteur = new Eleve();
-        correcteur = new Professeur();
+        this.note = NOTE_PAR_DEFAUT;
+        this.auteur = new Eleve();
+        this.correcteur = new Professeur();
     }
 
 
-    public Evaluation(Eleve pAuteur, Professeur pCorrecteur)
+    /**
+     * Instancie une évaluation à partir de son auteur {@link Eleve} et de son correcteur {@link Professeur}
+     * @param auteur
+     * @param correcteur
+     */
+    public Evaluation(Eleve auteur, Professeur correcteur)
     {
-        note = 10;
-        auteur = pAuteur;
-        correcteur = pCorrecteur;
+        this.note = NOTE_PAR_DEFAUT;
+        this.auteur = auteur;
+        this.correcteur = correcteur;
     }
 
 
+    /**
+     * Créé une représentation textuelle de l'instance
+     * @return Représentation textuelle de l'instance
+     */
     public String toString()
     {
+        // TODO: implement this method
         return "Cé Pô Implaimantay";
     }
 }
