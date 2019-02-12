14517794ee124b6f7f6b9ff9bf5a07a1a67db09c
Camille Briand <camille.briand@efrei.net>
2019-02-12 19:29:42 +0100

    Correction de la classe Promotion : - JavaDoc - Commentaires - Application de la norme

--- src/notesElevesProfesseurs/Promotion.java
+++ src/notesElevesProfesseurs/Promotion.java
@@ -3,28 +3,49 @@ package notesElevesProfesseurs;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+
+/**
+ * Ensemble d'{@link Eleve}s.
+ */
 public class Promotion
 {
-    ArrayList<Eleve> listeEleve;
-    String nom;
-    int annee;
+    private ArrayList<Eleve> listeEleve;
+    private String nom;
+    private int annee;
 
+
+    private static final String NOM_PAR_DEFAUT = "Promo Sion";
+    private static final int ANNEE_PAR_DEFAUT = 1234;
+
+
+    /**
+     * Constructeur par défaut
+     */
     public Promotion()
     {
-        listeEleve = new ArrayList<>();
-        nom = "Promo Sion";
-        annee = 1234;
+        this.listeEleve = new ArrayList<>();
+        this.nom = NOM_PAR_DEFAUT;
+        this.annee = ANNEE_PAR_DEFAUT;
     }
 
 
-    public Promotion(String pNom, int pAnnee)
+    /**
+     * Instancie une {@link Promotion} avec un nom de promotion et une année
+     * @param nom Nom de la promotion
+     * @param annee Année de la promotion
+     */
+    public Promotion(String nom, int annee)
     {
-        listeEleve = new ArrayList<>();
-        nom = pNom;
-        annee = pAnnee;
+        this.listeEleve = new ArrayList<>();
+        this.nom = nom;
+        this.annee = annee;
     }
 
 
+    /**
+     * Créé une représentation textuelle de l'instance
+     * @return Représentation textuelle de l'instance
+     */
     public String toString()
     {
         return "Cé Pô Implaimantay";
