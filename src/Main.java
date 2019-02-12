3def1041fb4b0165196d6d9761d4f95e90583a35
Camille Briand <camille.briand@efrei.net>
2019-02-12 19:30:59 +0100

    Supprime les instanciations de Personne dans la méthode main (la classe est devenue abstraite et ne peut plus être instanciée)

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,11 @@ public class Main
 {
     public static void main(String[] argv)
     {
-        Personne p1 = new Personne();
-        Personne p2 = new Personne("Jean" , "Kevin", new Date());
-
         Eleve E = new Eleve();
         Eleve E2 = new Eleve("Jean" , "névèle", new Date());
 
         Professeur P = new Professeur();
         Professeur P2 = new Professeur("Pro" , "Faiseur", new Date());
-
-
-
-        System.out.println(p1);
-        System.out.println(p2);
 
         System.out.println(E);
         System.out.println(E2);
