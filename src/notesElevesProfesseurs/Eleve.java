7401a509c2db1d37a69c8cd35a747261b1141a3c
Camille Briand <camille.briand@efrei.net>
2019-02-12 19:12:33 +0100

    Correction complète de la classe Eleve : - Respect des normes - JavaDoc - Commentaires - Réécriture de la méthode toString pour optimisation

--- src/notesElevesProfesseurs/Eleve.java
+++ src/notesElevesProfesseurs/Eleve.java
@@ -2,27 +2,49 @@ package notesElevesProfesseurs;
 
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Date;  // On favorise l'utilisation des classes déjà existantes en Java
 
 
+/**
+ * Classe représentant un élève.
+ *  Cette classe hérite de {@link Personne}
+ */
 public class Eleve extends Personne
 {
+    /**
+     * Constructeur par défaut : créé une instance vide
+     */
     public Eleve()
     {
         super();
     }
 
 
-    public Eleve(String pNom, String pPrenom, Date pDateNaissance)
+    /**
+     * Constructeur prenant en compte le nom, le prénom
+     * et la date de naissance de l'élève à instancier
+     * @param nom Nom de la {@link Personne} qui définit l'élève
+     * @param prenom Prénom de la {@link Personne} qui définit l'élève
+     * @param dateDeNaissance {@link Personne} qui définit l'élève
+     */
+    public Eleve(String nom, String prenom, Date dateDeNaissance)
     {
-        super(pNom, pPrenom, pDateNaissance);
+        super(nom, prenom, dateDeNaissance);
     }
 
 
+    /**
+     * Créé une représentation textuelle de l'instance
+     * @return Représentation textuelle de l'instance
+     */
     public String toString()
     {
-        return ID + "   #    Elève   # - : "
-                + prenom + " " + nom
-                + " , né le " + new SimpleDateFormat("dd MMM yyyy").format(dateNaissance);
+        return String.format(
+            "%s   #    Eleve   # - : %s %s, né le %s",
+            this.ID,
+            this.prenom,
+            this.nom,
+            new SimpleDateFormat("dd MM yyyy").format(this.dateNaissance)
+        );
     }
 }
